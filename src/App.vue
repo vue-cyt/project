eaacc0922b56f46e86c5f35a4aed1dc9c5cd2e57
ytchen <ytchen@example.com>
2022-05-06 21:00:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div>
+      <input v-model='inputValue' />
+      <button @click="handleClick">提交</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item, index) of list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="handleDelete($event)"
+      ></todo-item>
+    </ul>
+    <router-link to="/">1111</router-link>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./components/TodoItem";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      inputValue: "",
+      list: [],
+    };
+  },
+  components: {
+    "todo-item": TodoItem,
+  },
+  methods: {
+    handleClick() {
+      this.list.push(this.inputValue);
+      this.inputValue = "";
+    },
+    handleDelete(index) {
+      this.list.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li @click="handleClick">{{ content }}</li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["content", "index"],
+  methods: {
+    handleClick() {
+      this.$emit("delete", this.index);
+    },
+  },
+};
+</script>
